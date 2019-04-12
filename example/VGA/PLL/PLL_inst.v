3d5088d2cf43ea699a5b6a5622919c21e8d14233
Burak <Burak_yavuz97@hotmail.com>
2019-04-12 16:03:23 +0200

    VGA Test Burak
    Vga test from youtube video, "FPGA tutorial nr.4"

---
+++ example/VGA/PLL/PLL_inst.v
@@ -0,0 +1,6 @@
+	PLL u0 (
+		.clkin_clk   (<connected-to-clkin_clk>),   //  clkin.clk
+		.reset_reset (<connected-to-reset_reset>), //  reset.reset
+		.clkout_clk  (<connected-to-clkout_clk>)   // clkout.clk
+	);
+
