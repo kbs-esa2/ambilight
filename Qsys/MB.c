e97dfa626ba94d7a2abb4160d04705a47f6596cd
Joost <joosthartkamp@hotmail.com>
2019-04-18 14:15:41 +0200

    added sdram to niosII

--- Qsys/MB.c
+++ Qsys/MB.c
@@ -1,8 +1,19 @@
+#include <stdio.h>
+
 #define switches 	(volatile int *) 	0x00021000
-#define leds 		(int *) 		0x00021010
+#define leds 		(int *) 			0x00021010
+#define SDRAM		(volatile int *)	0x08000000
 
 void main(){
 	while(1){
-		*leds = *switches;
+		int command = (*switches & 0x030000)>>16;
+		int value = (*switches & 0xFFFF);
+		if(command&1){
+			*SDRAM = value;
+		}
+		else if(command&3){
+			*leds = *SDRAM;
+		}
+		
 	}
  }
