6b54eac14f3325313ba0d375694ca6d76b554e9f
Joost <joosthartkamp@hotmail.com>
2019-04-24 11:41:55 +0200

    new qsys

--- Qsys/MB.c
+++ Qsys/MB.c
@@ -1,19 +1,41 @@
 #include <stdio.h>
 
-#define switches 	(volatile int *) 	0x00021000
-#define leds 		(int *) 			0x00021010
+#define switches 	(volatile int *) 	0x00021030
+#define keys 	(volatile int *) 	0x00021020
+#define leds 		(int *) 			0x00021040
+
 #define SDRAM		(volatile int *)	0x08000000
+#define DMAINCONTROL		(volatile int *)	0x00021060
+#define DMAOUTCONTROL		(volatile int *)	0x00021070
+#define PIXELBUFFER		(volatile int *)	0x00200000
+
+#define STREAMCONTROL0 (volatile int *)	0x00021010
+#define STREAMCONTROL1 (volatile int *)	0x00021000
 
 void main(){
+	*leds = 0x00000000;
 	while(1){
+
 		int command = (*switches & 0x030000)>>16;
 		int value = (*switches & 0xFFFF);
-		if(command&1){
-			*SDRAM = value;
+		
+		if((*switches>>0)&1){
+			*leds |= (1<<0);
+			*STREAMCONTROL0 = 1;
 		}
-		if(command&3){
-			*leds = *SDRAM;
+		else{
+			*leds &= ~(1<<0);
+			*STREAMCONTROL0 = 0;
 		}
+		if((*switches>>1)&1){
+			*leds |= (1<<1);
+			*STREAMCONTROL1 = 1;
+		}
+		else{
+			*leds &= ~(1<<1);
+			*STREAMCONTROL1 = 0;
+		}
+		
 		
 	}
  }
