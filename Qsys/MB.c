0be17e3a79644b30f640c41f495da93c10ec09f3
joost hartkamp <joosthartkamp@hotmail.com>
2019-05-07 13:41:12 +0200

    brute 128K ram in je muil

--- Qsys/MB.c
+++ Qsys/MB.c
@@ -1,103 +1,91 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define switches 	(volatile int *) 	0x00021030
-#define keys 	(volatile int *) 	0x00021020
-#define leds 		(int *) 			0x00021040
-#define PLLSlave		(volatile int *)	0x00021050
+#define switches (volatile int *)0x08221010
+#define keys (volatile int *)0x08221000
+#define leds (int *)0x08221020
+#define PLLSlave (volatile int *)0x00021050
 
-#define niosMemorySlave		(volatile int *)	0x18000000
-#define DMAINCONTROL		(volatile int *)	0x00021060
-#define DMAOUTCONTROL		(volatile int *)	0x00021070
-#define sramBuffer		(volatile int *)	0x00000000
-#define sdramBuffer		(volatile int *)	0x08000000
+#define niosMemorySlave (volatile int *)0x18000000
+#define DMAINCONTROL (volatile int *)0x00021060
+#define DMAOUTCONTROL (volatile int *)0x00021070
+#define backBuffer (volatile unsigned char *)0x08040000
+#define frontBuffer (volatile unsigned char *)0x08000000
 
-#define AVConfigSlave		(volatile int *)	0x00021080
-#define JtagSlave		(volatile int *)	0x00021090
+#define AVConfigSlave (volatile int *)0x00021080
+#define JtagSlave (volatile int *)0x00021090
 
-#define bufferBypassSplit (volatile int *)	0x00021010
-#define bufferBypassMerge (volatile int *)	0x00021000
-#define SourceSelect (volatile int *)	0x000210a0
+#define FRAMEWIDTH 320
+#define FRAMEHEIGHT 240
 
-void setBufferInputAdress(unsigned int adress);
-void setBufferOutputAdress(unsigned int adress);
 
-void setInput(int input);
-void setBufferBypass(int input);
-
-void main(){
+void main()
+{
 	*leds = 0x00000000;
-	setBufferInputAdress(0x08000000);
-	setBufferOutputAdress(0x08100000);
-	while(1){
+	while (1)
+	{
+		int frameSize = FRAMEHEIGHT*FRAMEWIDTH;
 
-		int command = (*switches & 0x030000)>>16;
-		int value = (*switches & 0xFFFF);
-		
-		if((*switches>>0)&1){
-			*leds |= (1<<0);
-			setInput(1);
+		if ((*switches >> 0) & 1)
+		{
+			*leds |= (1 << 0);
+			for (int i = 0; i < 40; i++)
+			{
+				*(frontBuffer+i) = 255;
+				*(frontBuffer+i*3+1) = 0;
+				*(frontBuffer+i*3+2) = 0;
+			}
 		}
-		else{
-			*leds &= ~(1<<0);
-			setInput(0);
+		else
+		{
+			*leds &= ~(1 << 0);
 		}
-		if((*switches>>1)&1){
-			*leds |= (1<<1);
-			setBufferBypass(1);
+		if ((*switches >> 1) & 1)
+		{
+			*leds |= (1 << 1);
+			for (int i = 0; i < 40; i++)
+			{
+				*(frontBuffer+i) = 0;
+				*(frontBuffer+i*3+1) = 255;
+				*(frontBuffer+i*3+2) = 0;
+			}
 		}
-		else{
-			*leds &= ~(1<<1);
-			setBufferBypass(0);
+		else
+		{
+			*leds &= ~(1 << 1);
 		}
-		if((*switches>>2)&1){
-			setBufferInputAdress(0x08000000);
-			
+
+		if ((*switches >> 2) & 1)
+		{
+			*leds |= (1 << 2);
+			for (int i = 0; i < 40; i++)
+			{
+				*(backBuffer+i) = 255;
+				*(backBuffer+i+frameSize) = 0;
+				*(backBuffer+i+frameSize*2) = 0;
+			}
 		}
-		if((*switches>>3)&1){
-			setBufferInputAdress(0x08100000);
+		else
+		{
+			*leds &= ~(1 << 2);
 		}
-		if((*switches>>4)&1){
-			setBufferOutputAdress(0x08000000);
-			
+
+		if ((*switches >> 3) & 1)
+		{
+			*leds |= (1 << 3);
+			for (int i = 0; i < 40; i++)
+			{
+				*(backBuffer+i) = 0;
+				*(backBuffer+i+frameSize) = 255;
+				*(backBuffer+i+frameSize*2) = 0;
+			}
 		}
-		if((*switches>>5)&1){
-			setBufferOutputAdress(0x08100000);
+		else
+		{
+			*leds &= ~(1 << 3);
 		}
-		
 		
 	}
- }
+}
 
- void setBufferInputAdress(unsigned int adress){
-*(DMAINCONTROL+4) = adress;
-*DMAINCONTROL = 0;
-*(DMAINCONTROL+4) = adress;
- }
 
- void setBufferOutputAdress(unsigned int adress){
-	 *(DMAOUTCONTROL+4) = adress;
-*DMAOUTCONTROL = 0;
-*(DMAOUTCONTROL+4) = adress;
- }
-
- void setInput(int input){
-	 if (input == 1) {
-		 *SourceSelect = 1;
-	 }
-	 else{
-*SourceSelect = 0;
-	 }
-	 
- }
-
- void setBufferBypass(int input){
-	 if (input == 1) {
-		 *bufferBypassSplit = 1;
-		 *bufferBypassMerge = 1;
-	 }
-	 else{
-*bufferBypassSplit = 0;
-*bufferBypassMerge = 0;
-	 }
-	 
- }
