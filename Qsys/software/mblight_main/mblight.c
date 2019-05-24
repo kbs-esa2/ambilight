877f5cf7180fdccaa466fa8995be922d40859f4b
hansleon <leon.hans@live.nl>
2019-05-24 16:12:55 +0200

    brightness 2 - the sequel

--- Qsys/software/mblight_main/mblight.c
+++ Qsys/software/mblight_main/mblight.c
@@ -112,22 +112,26 @@ unsigned int emptyWidthRight = 0;
 
 /* Tasks */
 void TaskGetColor(void *pdata) {
-  color testc2 = {0, 0, 255, 255};
-  while (1) {
+//  color testc2 = {0, 0, 255, 255};
+//  while (1) {
+//
+//    /*  TEST */
+//    color testc = {129, 171, 239, 255};
+//    byte testx = 160;
+//    byte testy = 120;
+//
+//    setPixel(testx-1, testy, testc);
+//    setPixel(testx+1, testy, testc);
+//    setPixel(testx, testy-1, testc);
+//    setPixel(testx, testy+1, testc);
+//    readPixel = getPixelColor(testx, testy);
+//    for (byte i = 0; i < 96; i++) setLed(i, testc2);
+//    //testc2.red++;
+//    if(testc2.red == 255) testc2.red = 0;
+	while(1){
+		getAverages();
+		averageToBlocks();
 
-    /*  TEST */
-    color testc = {129, 171, 239, 255};
-    byte testx = 160;
-    byte testy = 120;
-
-    setPixel(testx-1, testy, testc);
-    setPixel(testx+1, testy, testc);
-    setPixel(testx, testy-1, testc);
-    setPixel(testx, testy+1, testc);
-    readPixel = getPixelColor(testx, testy);
-    for (byte i = 0; i < 96; i++) setLed(i, testc2);
-    //testc2.red++;
-    if(testc2.red == 255) testc2.red = 0;
     //printf("r:%d, g:%d, b:%d\n", testc2.red, testc2.green, testc2.blue);
     /* END TEST */
 
@@ -438,9 +442,9 @@ void getAverages() {
 				totalB += temp.blue;
 			}
 		}
-		leftEdge.frameBlock[i].average.red = (totalR * (*(brightnessBuffer) + 1) / 16) / (leftEdge.frameBlock[i].Height * leftEdge.frameBlock[i].Width);
-		leftEdge.frameBlock[i].average.green = (totalG * (*(brightnessBuffer) + 1) / 16) / (leftEdge.frameBlock[i].Height * leftEdge.frameBlock[i].Width);
-		leftEdge.frameBlock[i].average.blue = (totalB * (*(brightnessBuffer) + 1) / 16) / (leftEdge.frameBlock[i].Height * leftEdge.frameBlock[i].Width);
+		leftEdge.frameBlock[i].average.red = totalR / (leftEdge.frameBlock[i].Height * leftEdge.frameBlock[i].Width);
+		leftEdge.frameBlock[i].average.green = totalG / (leftEdge.frameBlock[i].Height * leftEdge.frameBlock[i].Width);
+		leftEdge.frameBlock[i].average.blue = totalB/ (leftEdge.frameBlock[i].Height * leftEdge.frameBlock[i].Width);
 
 		totalR = 0;
 		totalG = 0;
@@ -456,9 +460,9 @@ void getAverages() {
 				totalB += temp.blue;
 			}
 		}
-		topEdge.frameBlock[i].average.red = (totalR * (*(brightnessBuffer) + 1) / 16) / (topEdge.frameBlock[i].Height * topEdge.frameBlock[i].Width);
-		topEdge.frameBlock[i].average.green = (totalG * (*(brightnessBuffer) + 1) / 16) / (topEdge.frameBlock[i].Height * topEdge.frameBlock[i].Width);
-		topEdge.frameBlock[i].average.blue = (totalB * (*(brightnessBuffer) + 1) / 16) / (topEdge.frameBlock[i].Height * topEdge.frameBlock[i].Width);
+		topEdge.frameBlock[i].average.red = totalR/ (topEdge.frameBlock[i].Height * topEdge.frameBlock[i].Width);
+		topEdge.frameBlock[i].average.green = totalG / (topEdge.frameBlock[i].Height * topEdge.frameBlock[i].Width);
+		topEdge.frameBlock[i].average.blue = totalB / (topEdge.frameBlock[i].Height * topEdge.frameBlock[i].Width);
 
     totalR = 0;
     totalG = 0;
@@ -474,9 +478,9 @@ void getAverages() {
 				totalB += temp.blue;
 			}
 		}
-		rightEdge.frameBlock[i].average.red = (totalR * (*(brightnessBuffer) + 1) / 16) / (rightEdge.frameBlock[i].Height * rightEdge.frameBlock[i].Width);
-		rightEdge.frameBlock[i].average.green = (totalG * (*(brightnessBuffer) + 1) / 16) / (rightEdge.frameBlock[i].Height * rightEdge.frameBlock[i].Width);
-		rightEdge.frameBlock[i].average.blue = (totalB * (*(brightnessBuffer) + 1) / 16) / (rightEdge.frameBlock[i].Height * rightEdge.frameBlock[i].Width);
+		rightEdge.frameBlock[i].average.red = totalR / (rightEdge.frameBlock[i].Height * rightEdge.frameBlock[i].Width);
+		rightEdge.frameBlock[i].average.green = totalG / (rightEdge.frameBlock[i].Height * rightEdge.frameBlock[i].Width);
+		rightEdge.frameBlock[i].average.blue = totalB / (rightEdge.frameBlock[i].Height * rightEdge.frameBlock[i].Width);
 
 		totalR = 0;
 		totalG = 0;
@@ -492,9 +496,9 @@ void getAverages() {
 				totalB += temp.blue;
 			}
 		}
-		bottomEdge.frameBlock[i].average.red = (totalR * (*(brightnessBuffer) + 1) / 16) / (bottomEdge.frameBlock[i].Height * bottomEdge.frameBlock[i].Width);
-		bottomEdge.frameBlock[i].average.green = (totalG * (*(brightnessBuffer) + 1) / 16) / (bottomEdge.frameBlock[i].Height * bottomEdge.frameBlock[i].Width);
-		bottomEdge.frameBlock[i].average.blue = (totalB * (*(brightnessBuffer) + 1) / 16) / (bottomEdge.frameBlock[i].Height * bottomEdge.frameBlock[i].Width);
+		bottomEdge.frameBlock[i].average.red = totalR / (bottomEdge.frameBlock[i].Height * bottomEdge.frameBlock[i].Width);
+		bottomEdge.frameBlock[i].average.green = totalG / (bottomEdge.frameBlock[i].Height * bottomEdge.frameBlock[i].Width);
+		bottomEdge.frameBlock[i].average.blue = totalB / (bottomEdge.frameBlock[i].Height * bottomEdge.frameBlock[i].Width);
 
     totalR = 0;
     totalG = 0;
@@ -502,11 +506,33 @@ void getAverages() {
   }
 }
 
+
+
 void averageToLeds(){
-	for(unsigned int i = 0 ; i < leftEdge.numLeds; i++ ) setLed( leftEdge.frameBlock[i].id , leftEdge.frameBlock[i].average );
-	for(unsigned int i = 0 ; i < topEdge.numLeds; i++ ) setLed( topEdge.frameBlock[i].id , topEdge.frameBlock[i].average );
-	for(unsigned int i = 0 ; i < rightEdge.numLeds; i++ ) setLed( rightEdge.frameBlock[i].id , rightEdge.frameBlock[i].average );
-	for(unsigned int i = 0 ; i < bottomEdge.numLeds; i++ ) setLed( bottomEdge.frameBlock[i].id , bottomEdge.frameBlock[i].average );
+	for(unsigned int i = 0 ; i < leftEdge.numLeds; i++ ){
+		leftEdge.frameBlock[i].average.red = (leftEdge.frameBlock[i].average.red * (*(brightnessBuffer) + 1)) / 16;
+		leftEdge.frameBlock[i].average.green = (leftEdge.frameBlock[i].average.green * (*(brightnessBuffer) + 1)) / 16;
+		leftEdge.frameBlock[i].average.blue = (leftEdge.frameBlock[i].average.blue * (*(brightnessBuffer) + 1)) / 16;
+		setLed( leftEdge.frameBlock[i].id , leftEdge.frameBlock[i].average );
+	}
+	for(unsigned int i = 0 ; i < topEdge.numLeds; i++ ){
+		topEdge.frameBlock[i].average.red = (topEdge.frameBlock[i].average.red * (*(brightnessBuffer) + 1)) / 16;
+		topEdge.frameBlock[i].average.green = (topEdge.frameBlock[i].average.green * (*(brightnessBuffer) + 1)) / 16;
+		topEdge.frameBlock[i].average.blue = (topEdge.frameBlock[i].average.blue * (*(brightnessBuffer) + 1)) / 16;
+		setLed( topEdge.frameBlock[i].id , topEdge.frameBlock[i].average );
+	}
+	for(unsigned int i = 0 ; i < rightEdge.numLeds; i++ ){
+		rightEdge.frameBlock[i].average.red = (rightEdge.frameBlock[i].average.red * (*(brightnessBuffer) + 1)) / 16;
+		rightEdge.frameBlock[i].average.green = (rightEdge.frameBlock[i].average.green * (*(brightnessBuffer) + 1)) / 16;
+		rightEdge.frameBlock[i].average.blue = (rightEdge.frameBlock[i].average.blue * (*(brightnessBuffer) + 1)) / 16;
+		setLed( rightEdge.frameBlock[i].id , rightEdge.frameBlock[i].average );
+	}
+	for(unsigned int i = 0 ; i < bottomEdge.numLeds; i++ ){
+		bottomEdge.frameBlock[i].average.red = (bottomEdge.frameBlock[i].average.red * (*(brightnessBuffer) + 1)) / 16;
+		bottomEdge.frameBlock[i].average.green = (bottomEdge.frameBlock[i].average.green * (*(brightnessBuffer) + 1)) / 16;
+		bottomEdge.frameBlock[i].average.blue = (bottomEdge.frameBlock[i].average.blue * (*(brightnessBuffer) + 1)) / 16;
+		setLed( bottomEdge.frameBlock[i].id , bottomEdge.frameBlock[i].average );
+	}
 }
 
 void averageToBlocks(){
